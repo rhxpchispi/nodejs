d5b0c204a27db6cdb24260c0010be54134d4512f
rhxpchispi <cristianarieljuarez@gmail.com>
2025-08-10 19:33:20 -0300

    AGREGO SEGUNDO PIPE

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = "0.0.0.0";
 // App
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hola Mundo que nos vió en esta app 3.0.");
+  res.send("Hola Mundo que nos ve en AWS!!");
 });
 
 app.listen(PORT, HOST, () => {
